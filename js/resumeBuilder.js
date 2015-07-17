2b367ba9b8a0304e264931d67463aa83fbf1058b
cpiyush <cpiyush100@gmail.com>
2015-07-17 06:41:35 -0700

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var awesome = "AWESOME";
+ var awesomeThoughts = replace("AWESOME,AWESOME THOUGHTS");
+ console.log(awesome);
+ console.log(awesomeThoughts)
